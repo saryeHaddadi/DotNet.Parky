3022d0d536b7042bc1c4c54123e15b23b9efbf96
sarye.haddadi <sarye@live.fr>
2022-02-09 10:37:23 +0100

    Init ParkyWeb

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace ParkyWeb;
+
+public static class SD
+{
+	public static string ApiBaseUrl = "https://localhost:7299";
+	public static string NationalParkApiPath = ApiBaseUrl + "api/v1/nationalparks";
+	public static string TrailApiPath = ApiBaseUrl + "api/v1/trails";
+}
