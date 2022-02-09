5bc2f9146f7e3e1a2e5f772182c8ad926b89cda0
sarye.haddadi <sarye@live.fr>
2022-02-09 11:58:44 +0100

    svg

---
+++ ParkyWeb/Controllers/NationalParkController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.Interface;
+
+namespace ParkyWeb.Controllers;
+public class NationalParkController : Controller
+{
+	private readonly INationalParkRepository _npRepo;
+	public NationalParkController(INationalParkRepository npRepo)
+	{
+		_npRepo = npRepo;
+	}
+	public IActionResult Index()
+	{
+		return View(new NationalPark() { });
+	}
+
+	public async Task<IActionResult> GetAllNationalPark()
+	{
+		return Json(new { data = await _npRepo.GetAllAsync(SD.NationalParkApiPath) });
+	}
+}
