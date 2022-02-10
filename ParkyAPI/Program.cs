48eace2672d87587e85623bd5ce9c50541976928
sarye.haddadi <sarye@live.fr>
2022-02-10 08:35:45 +0100

    svg

--- ParkyAPI/Program.cs
+++ ParkyAPI/Program.cs
@@ -17,6 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<INationalParkRepository, NationalParkRepository>();
 builder.Services.AddScoped<ITrailRepository, TrailRepository>();
 builder.Services.AddAutoMapper(typeof(ParkyMappings));
